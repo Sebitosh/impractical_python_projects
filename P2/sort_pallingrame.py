6d35faa0db63a986d1ed506826263eee4ff105a8
Sebitosh <sebastien111@hotmail.be>
2022-05-15 23:02:26 +0200

    test

--- P2/sort_pallingrame.py
+++ P2/sort_pallingrame.py
@@ -3,6 +3,7 @@
 """
 Objective Use Python to search an English language dictionary for two-word palingrams. Analyze
 and optimize the palingram code using the cProfile too
+test
 """
 
 import load_dictionary
